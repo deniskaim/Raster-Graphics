fd6f8fbed37864cc1270f7f7897e4461523c9773
Denis Kaim <139626539+WarriorCod24@users.noreply.github.com>
2024-06-23 15:04:20 +0300

    organise FactoriesFolder

---
+++ Raster Graphics/Factories/CommandFactory.h
@@ -0,0 +1,20 @@
+#pragma once
+#include "Command.h"
+#include "Polymorphic_ptr.hpp"
+#include <fstream>
+
+class CommandFactory
+{
+public:
+	static Polymorphic_ptr<Command> createCommand();
+
+private:
+	static Polymorphic_ptr<Command> createLoadCommand(std::stringstream& ss);
+	static Polymorphic_ptr<Command> createSaveAsCommand(std::stringstream& ss);
+	static Polymorphic_ptr<Command> createRotateCommand(std::stringstream& ss);
+	static Polymorphic_ptr<Command> createAddCommand(std::stringstream& ss);
+	static Polymorphic_ptr<Command> createSessionInfoCommand(std::stringstream& ss);
+	static Polymorphic_ptr<Command> createSwitchSessionCommand(std::stringstream& ss);
+	static Polymorphic_ptr<Command> createCollageCommand(std::stringstream& ss);
+
+};
